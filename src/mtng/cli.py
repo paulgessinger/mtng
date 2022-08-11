c363b9ba987f1efb59d4aa379ca99a4ee901cc34
Paul Gessinger <hello@paulgessinger.com>
2022-08-11 12:45:20 +0200

    refactor: Refactor the code a bit, add minimal tests + CI  (#6)

--- src/mtng/cli.py
+++ src/mtng/cli.py
@@ -14,11 +14,13 @@ import gidgethub
 import aiohttp
 import dateutil.parser
 import yaml
+from mtng.generate import generate_latex
 
 from mtng.spec import Spec
+from mtng.collect import collect_repositories
 from mtng import __version__
 
-load_dotenv(dotenv_path=Path.cwd() /".env")
+load_dotenv(dotenv_path=Path.cwd() / ".env")
 
 cli = typer.Typer()
 
@@ -64,9 +66,9 @@ async def collect(gh: GitHubAPI, repo: str, dt: datetime.datetime):
 async def handle_event(event: str, session):
     contributions = []
 
-    indico_id = re.match(r"https://indico.cern.ch/event/(\d*)/?", event).group(1)
+    indico_host, indico_id = re.match(r"https://(.*)/event/(\d*)/?", event).groups()
     async with session.get(
-        f"https://indico.cern.ch/export/event/{indico_id}.json?detail=contributions",
+        f"https://{indico_host}/export/event/{indico_id}.json?detail=contributions",
     ) as res:
         event = await res.json()
 
@@ -100,7 +102,7 @@ async def handle_event(event: str, session):
 async def generate(
     config: typer.FileText,
     token: str = typer.Option(
-        os.environ.get("GH_TOKEN",...),
+        os.environ.get("GH_TOKEN", ...),
         help="Github API token to use. Can be supplied with environment variable GH_TOKEN",
     ),
     since: datetime.datetime = typer.Option(
@@ -120,80 +122,12 @@ async def generate(
             contributions = handle_event(event, session)
 
         gh = GitHubAPI(session, __name__, oauth_token=token)
-        data = {}
-        for repo in spec.repos:
-            # open_prs, merged_prs, stale = await collect(gh, repo, dt)
-            data[repo.name] = {}
-            data[repo.name]["merged_prs"] = []
-            data[repo.name]["open_prs"] = []
-            data[repo.name]["stale"] = []
-            data[repo.name]["recent_issues"] = []
-            data[repo.name]["spec"] = repo
 
-            if repo.do_merged_prs:
-                merged_prs = [
-                    gh.getitem(f"/repos/{repo.name}/pulls/{issue['number']}")
-                    async for issue in gh.getiter(
-                        f"/search/issues?q=repo:{repo.name}+is:pr+merged:{since:%Y-%m-%d}..{now:%Y-%m-%d}",
-                    )
-                ]
-                data[repo.name]["merged_prs"] = merged_prs
-
-            if repo.do_open_prs:
-                url = f"/search/issues?q=repo:{repo.name}+is:pr+is:open"
-                if repo.wip_label is not None:
-                    url += f'+-label:"{urllib.parse.quote(repo.wip_label)}"'
-                    # url += '+-label:"{repo.wip_label}"'
-                open_prs = [
-                    gh.getitem(f"/repos/{repo.name}/pulls/{issue['number']}")
-                    async for issue in gh.getiter(url)
-                ]
-                data[repo.name]["open_prs"] = open_prs
-
-            if repo.do_stale:
-                stale = [
-                    issue
-                    async for issue in gh.getiter(
-                        f'/search/issues?q=repo:{repo.name}+is:open+label:"{urllib.parse.quote(repo.stale_label)}"'
-                    )
-                ]
-
-                data[repo.name]["stale"] = stale
-
-            if repo.do_recent_issues:
-                url = f"/search/issues?q=repo:{repo.name}+is:open+is:issue+created:{since:%Y-%m-%d}..{now:%Y-%m-%d}"
-                #  print(url)
-                recent_issues = [issue async for issue in gh.getiter(url)]
-                data[repo.name]["recent_issues"] = recent_issues
-
-            for prk in "open_prs", "merged_prs":
-                data[repo.name][prk] = await asyncio.gather(*data[repo.name][prk])
-
-            for prk in "open_prs", "merged_prs", "stale", "recent_issues":
-                for pr in data[repo.name][prk]:
-                    for k in "merged_at", "updated_at":
-                        if not k in pr:
-                            continue
-                        pr[k] = (
-                            dateutil.parser.parse(pr[k]).replace(tzinfo=None) if pr[k] is not None else None
-                        )
+        data = await collect_repositories(spec.repos, gh=gh, since=since, now=now)
 
         contributions = await contributions if event is not None else []
 
-    from jinja2 import Environment, FileSystemLoader
-
-    env = Environment(
-        loader=FileSystemLoader(Path(__file__).parent / "template"),
-    )
-
-    def sanitize(s):
-        return s.replace("_", "\\_").replace("#", "\\#").replace("&", "\\&")
-
-    env.filters["sanitize"] = sanitize
-
-    tpl = env.get_template("main.tex")
-
-    print(tpl.render(repos=data, spec=spec, last=since, contributions=contributions))
+    print(generate_latex(spec, data, since, contributions))
 
 
 @cli.callback()
@@ -203,5 +137,6 @@ def main():
 
 main.__doc__ = """
 Meeting generation script, version {version}
-""".format(version=__version__)
-
+""".format(
+    version=__version__
+)
---
+++ src/mtng/collect.py
@@ -0,0 +1,100 @@
+from typing import List, Optional, Literal
+from datetime import datetime
+import urllib.parse
+import asyncio
+import dateutil.parser
+
+from gidgethub.abc import GitHubAPI
+
+from mtng.spec import Repository
+
+
+async def get_merged_pulls(
+    gh: GitHubAPI, repo_name: str, start: datetime, end: datetime
+):
+    return await asyncio.gather(
+        *[
+            gh.getitem(f"/repos/{repo_name}/pulls/{issue['number']}")
+            async for issue in gh.getiter(
+                f"/search/issues?q=repo:{repo_name}+is:pr+merged:{start:%Y-%m-%d}..{end:%Y-%m-%d}",
+            )
+        ]
+    )
+
+
+async def get_open_issues(
+    gh: GitHubAPI,
+    repo_name: str,
+    with_labels: List[str] = [],
+    without_labels: List[str] = [],
+    start: Optional[datetime] = None,
+    end: Optional[datetime] = None,
+    type: Literal["pr", "issue"] = "issue",
+):
+    if not all([start is None, end is None]) and not all(
+        [start is not None, end is not None]
+    ):
+        raise ValueError("Either provide start and end or neither")
+    url = f"/search/issues?q=repo:{repo_name}+is:open"
+    url += f"+is:{type}"
+    if start is not None and end is not None:
+        url += f"+created:{start:%Y-%m-%d}..{end:%Y-%m-%d}"
+    for label in without_labels:
+        url += f'+-label:"{urllib.parse.quote(label)}"'
+    for label in with_labels:
+        url += f'+label:"{urllib.parse.quote(label)}"'
+    return await asyncio.gather(
+        *[
+            gh.getitem(f"/repos/{repo_name}/issues/{issue['number']}")
+            async for issue in gh.getiter(url)
+        ]
+    )
+
+
+async def collect_repositories(
+    repos: List[Repository], since: datetime, now: datetime, gh: GitHubAPI
+):
+    data = {}
+    for repo in repos:
+        data[repo.name] = {}
+        data[repo.name]["merged_prs"] = []
+        data[repo.name]["open_prs"] = []
+        data[repo.name]["stale"] = []
+        data[repo.name]["recent_issues"] = []
+        data[repo.name]["spec"] = repo
+
+        if repo.do_merged_prs:
+            merged_prs = await get_merged_pulls(gh, repo.name, since, now)
+            data[repo.name]["merged_prs"] = merged_prs
+
+        if repo.do_open_prs:
+            data[repo.name]["open_prs"] = await get_open_issues(
+                gh,
+                repo.name,
+                without_labels=[repo.wip_label] if repo.wip_label is not None else [],
+                type="pr",
+            )
+
+        if repo.do_stale:
+            if repo.stale_label is None:
+                raise ValueError("Provide stale label if do_stale=True")
+            stale = await get_open_issues(gh, repo.name, with_labels=[repo.stale_label])
+
+            data[repo.name]["stale"] = stale
+
+        if repo.do_recent_issues:
+            recent_issues = await get_open_issues(gh, repo.name, start=since, end=now)
+
+            data[repo.name]["recent_issues"] = recent_issues
+
+        for prk in "open_prs", "merged_prs", "stale", "recent_issues":
+            for pr in data[repo.name][prk]:
+                for k in "merged_at", "updated_at":
+                    if not k in pr:
+                        continue
+                    pr[k] = (
+                        dateutil.parser.parse(pr[k]).replace(tzinfo=None)
+                        if pr[k] is not None
+                        else None
+                    )
+    return data
