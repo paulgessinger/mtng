bfc2156c3227146ead50ec3ab023710bc63bdaff
Paul Gessinger <hello@paulgessinger.com>
2022-08-11 12:21:36 +0200

    factorize

--- src/mtng/cli.py
+++ src/mtng/cli.py
@@ -14,12 +14,13 @@ import gidgethub
 import aiohttp
 import dateutil.parser
 import yaml
+from mtng.generate import generate_latex
 
 from mtng.spec import Spec
 from mtng.collect import collect_repositories
 from mtng import __version__
 
-load_dotenv(dotenv_path=Path.cwd() /".env")
+load_dotenv(dotenv_path=Path.cwd() / ".env")
 
 cli = typer.Typer()
 
@@ -65,7 +66,7 @@ async def collect(gh: GitHubAPI, repo: str, dt: datetime.datetime):
 async def handle_event(event: str, session):
     contributions = []
 
-    indico_host,indico_id = re.match(r"https://(.*)/event/(\d*)/?", event).groups()
+    indico_host, indico_id = re.match(r"https://(.*)/event/(\d*)/?", event).groups()
     async with session.get(
         f"https://{indico_host}/export/event/{indico_id}.json?detail=contributions",
     ) as res:
@@ -101,7 +102,7 @@ async def handle_event(event: str, session):
 async def generate(
     config: typer.FileText,
     token: str = typer.Option(
-        os.environ.get("GH_TOKEN",...),
+        os.environ.get("GH_TOKEN", ...),
         help="Github API token to use. Can be supplied with environment variable GH_TOKEN",
     ),
     since: datetime.datetime = typer.Option(
@@ -126,20 +127,7 @@ async def generate(
 
         contributions = await contributions if event is not None else []
 
-    from jinja2 import Environment, FileSystemLoader
-
-    env = Environment(
-        loader=FileSystemLoader(Path(__file__).parent / "template"),
-    )
-
-    def sanitize(s):
-        return s.replace("_", "\\_").replace("#", "\\#").replace("&", "\\&")
-
-    env.filters["sanitize"] = sanitize
-
-    tpl = env.get_template("main.tex")
-
-    print(tpl.render(repos=data, spec=spec, last=since, contributions=contributions))
+    print(generate_latex(spec, data, since, contributions))
 
 
 @cli.callback()
@@ -149,5 +137,6 @@ def main():
 
 main.__doc__ = """
 Meeting generation script, version {version}
-""".format(version=__version__)
-
+""".format(
+    version=__version__
+)
---
+++ src/mtng/generate.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+from mtng.spec import Spec
+
+
+def generate_latex(spec: Spec, data, last: datetime, contributions) -> str:
+
+    env = Environment(
+        loader=FileSystemLoader(Path(__file__).parent / "template"),
+    )
+
+    def sanitize(s):
+        return s.replace("_", "\\_").replace("#", "\\#").replace("&", "\\&")
+
+    env.filters["sanitize"] = sanitize
+
+    tpl = env.get_template("main.tex")
+
+    return tpl.render(repos=data, spec=spec, last=last, contributions=contributions)
