de157f7ca2140a6ca1adb925b1aac1addf2991ec
Paul Gessinger <hello@paulgessinger.com>
2022-08-18 00:06:32 +0200

    feat: Enable emoji if lualatex is present (#12)

--- src/mtng/cli.py
+++ src/mtng/cli.py
@@ -44,6 +44,18 @@ def find_latexmk() -> Path:
     return latexmk_path
 
 
+def have_lualatex() -> bool:
+    try:
+        latexmk_path = Path(
+            subprocess.check_output(["which", "lualatex"]).decode().strip()
+        )
+    except subprocess.CalledProcessError:
+        return False
+    if not latexmk_path.exists():
+        return False
+    return True
+
+
 def make_sync(fn):
     @functools.wraps(fn)
     def wrapped(*args, **kwargs):
@@ -187,16 +199,17 @@ async def generate(
             d = Path(d)
             source = d / "source.tex"
             source.write_text(latex)
-            subprocess.check_call(
-                [
-                    latexmk,
-                    f"-output-directory={d}",
-                    "-halt-on-error",
-                    # "-pdflatex=lualatex",
-                    "-pdf",
-                    source,
-                ]
-            )
+            args = [
+                latexmk,
+                f"-output-directory={d}",
+                "-halt-on-error",
+                "-pdf",
+            ]
+
+            if have_lualatex():
+                args.append("-pdflatex=lualatex")
+            args.append(source)
+            subprocess.check_call(args)
             shutil.copy(d / "source.pdf", pdf)
 
 
