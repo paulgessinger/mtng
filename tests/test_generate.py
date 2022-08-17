4799b4b69e10e78003620ae4d952563a2ddbe5f3
Paul Gessinger <hello@paulgessinger.com>
2022-08-17 10:45:51 +0200

    compile unit test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,6 +5,7 @@ import json
 from pathlib import Path
 import os
 import warnings
+import subprocess
 
 import pytest
 import aiohttp
@@ -82,3 +83,76 @@ async def test_collect():
         result = await mtng.collect.collect_repositories(
             [repo], gh=gh, since=datetime(2022, 8, 1), now=datetime(2022, 8, 2)
         )
+
+
+# check if we have latexmk
+have_latexmk = False
+try:
+    latexmk_path = Path(subprocess.check_output(["which", "latexmk"]).decode().strip())
+    if latexmk_path.exists():
+        have_latexmk = True
+except:
+    pass
+
+
+@pytest.mark.skipif(not have_latexmk, reason="latexmk not found")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("full_tex", [True, False], ids=["full", "fragment"])
+async def test_compile(monkeypatch, full_tex, tmp_path):
+    gh = Mock()
+
+    repo = Repository(
+        name="acts-project/acts",
+        do_stale=True,
+        stale_label="Stale",
+        wip_label=":construction: WIP",
+    )
+
+    ref = Path(__file__).parent / "ref"
+
+    def get_file_content(file: str):
+        f = asyncio.Future()
+        with (ref / file).open() as fh:
+            f.set_result(json.load(fh))
+        return f
+
+    monkeypatch.setattr(
+        "mtng.collect.get_merged_pulls",
+        Mock(return_value=get_file_content("merged_prs.json")),
+    )
+    monkeypatch.setattr(
+        "mtng.collect.get_open_issues",
+        Mock(
+            side_effect=[
+                get_file_content("open_prs.json"),
+                get_file_content("stale.json"),
+                get_file_content("recent_issues.json"),
+            ]
+        ),
+    )
+    since = datetime(2022, 8, 1)
+    result = await mtng.collect.collect_repositories(
+        [repo], since=since, now=datetime(2022, 8, 11), gh=gh
+    )
+
+    tex = generate_latex(
+        Spec(repos=[repo]), result, last=since, contributions=[], full_tex=full_tex
+    )
+    source = tmp_path / "source.tex"
+
+    with source.open("w") as fh:
+        if not full_tex:
+            fh.write("\\documentclass{beamer}\n\\begin{document}\n")
+        fh.write(tex)
+        if not full_tex:
+            fh.write("\n\\end{document}")
+
+    subprocess.check_call(
+        [
+            latexmk_path,
+            f"-output-directory={tmp_path/'build'}",
+            "-halt-on-error",
+            "-pdf",
+            str(source),
+        ]
+    )
